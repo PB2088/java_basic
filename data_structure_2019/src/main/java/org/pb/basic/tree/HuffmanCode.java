ecda94c3c2a938747cb52f693aabfc8250abdcb3
PB915932857 <PB915932857@_@__>
2019-09-24 16:37:08 +0800

    赫夫曼解压

--- data_structure_2019/src/main/java/org/pb/basic/tree/HuffmanCode.java
+++ data_structure_2019/src/main/java/org/pb/basic/tree/HuffmanCode.java
@@ -9,17 +9,25 @@ import java.util.*;
 public class HuffmanCode {
     private static final Map<Byte, String> HUFF_MAN_CODE_MAP = new HashMap();
 
+    private static final Map<String, Byte> HUFF_MAN_NODE_CODE_MAP = new HashMap();
+
     public static void main(String[] args) {
         String content = "i like like like java do you like a java";
         byte[] contentBytes = content.getBytes();
-        System.out.println(contentBytes.length);
 
         HuffmanCode huffmanCode = new HuffmanCode();
-        System.out.println(Arrays.toString(huffmanCode.huffmanZip(contentBytes)));
+
+        byte[] huffmanBytes = huffmanCode.huffmanZip(contentBytes);
+
+        System.out.println(Arrays.toString(huffmanBytes));
+
+        byte[] bytes = huffmanCode.decode(huffmanBytes);
+        System.out.println(new String(bytes));
     }
 
     /**
      * 赫曼编码压缩
+     *
      * @param contentBytes 待压缩的字节数组
      * @return 压缩后的字节数组
      */
@@ -29,7 +37,7 @@ public class HuffmanCode {
 
         getHuffmanCodes(huffmanTree.getRoot(), "", new StringBuilder());
 
-        return zip(contentBytes,HUFF_MAN_CODE_MAP);
+        return zip(contentBytes, HUFF_MAN_CODE_MAP);
     }
 
     /**
@@ -76,12 +84,20 @@ public class HuffmanCode {
                 getHuffmanCodes(node.getRightChild(), "1", codeStringBuilder);
             } else {
                 HUFF_MAN_CODE_MAP.put((Byte) node.getData(), codeStringBuilder.toString());
+                HUFF_MAN_NODE_CODE_MAP.put(codeStringBuilder.toString(), (Byte) node.getData());
             }
         }
 
         return HUFF_MAN_CODE_MAP;
     }
 
+    /**
+     * 根据赫夫曼编码表进行字节压缩
+     *
+     * @param bytes
+     * @param huffmanCodes
+     * @return
+     */
     private byte[] zip(byte[] bytes, Map<Byte, String> huffmanCodes) {
         StringBuilder stringBuilder = new StringBuilder();
         for (byte aByte : bytes) {
@@ -101,5 +117,52 @@ public class HuffmanCode {
         }
 
         return huffmanCodeBytes;
+    }
+
+    public byte[] decode(byte[] huffmanBytes) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < huffmanBytes.length; i++) {
+            stringBuilder.append(byteToBitString(huffmanBytes[i]));
+        }
+
+        List<Byte> byteList = new ArrayList<>();
+        for (int i = 0; i < stringBuilder.length(); ) {
+            int step = 1;
+            boolean isRunning = true;
+            while (isRunning) {
+                String key = stringBuilder.substring(i, i + step);
+                Byte b = HUFF_MAN_NODE_CODE_MAP.get(key);
+
+                if (Objects.nonNull(b)) {
+                    byteList.add(b);
+
+                    isRunning = false;
+                    i = i + step;
+                } else {
+                    step++;
+                }
+            }
+        }
+
+        byte[] bytes = new byte[byteList.size()];
+        for (int i = 0; i < byteList.size(); i++) {
+            bytes[i] = byteList.get(i);
+        }
+
+        return bytes;
+    }
+
+    /**
+     * 字节转为二进制字符串
+     *
+     * @param b
+     * @return
+     */
+    private String byteToBitString(byte b) {
+        return ""
+                + (byte) ((b >> 7) & 0x1) + (byte) ((b >> 6) & 0x1)
+                + (byte) ((b >> 5) & 0x1) + (byte) ((b >> 4) & 0x1)
+                + (byte) ((b >> 3) & 0x1) + (byte) ((b >> 2) & 0x1)
+                + (byte) ((b >> 1) & 0x1) + (byte) ((b >> 0) & 0x1);
     }
 }
