37a84460521b29ad573f1b64624c7a0bb7050939
PB915932857 <PB915932857@_@__>
2019-11-14 17:29:25 +0800

    图

---
+++ data_structure_2019/src/main/java/org/pb/basic/graph/Graph.java
@@ -0,0 +1,51 @@
+package org.pb.basic.graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 图
+ * @author bo.peng
+ * @create 2019-11-14 16:38
+ */
+public class Graph {
+    /** 顶点列表 */
+    private List<String> vertexs;
+
+    /** 邻结矩阵 */
+    private int[][] edges;
+
+    /** 表示边的数目 */
+    private int numOfEdge;
+
+    /** 默认大小 */
+    public static final int DEFAULT_SIZE = 20;
+
+    public Graph(int size) {
+        int workSize = size < 0 ? DEFAULT_SIZE : size;
+
+        edges = new int[workSize][workSize];
+        vertexs = new ArrayList<>(workSize);
+    }
+
+    /**
+     * 增加一条边
+     * @param start 边的起始位置
+     * @param end 边的结束位置
+     * @param weight 边的权重
+     */
+    public void addEdge(int start,int end,int weight) {
+        edges[start][end] = weight;
+        edges[end][start] = weight;
+
+        numOfEdge ++;
+    }
+
+    public void showGraph() {
+        for (int[] edge : edges) {
+            System.out.println(Arrays.toString(edge));
+        }
+    }
+
+}
---
+++ data_structure_2019/src/test/java/org/pb/basic/graph/GraphTest.java
@@ -0,0 +1,8 @@
+package org.pb.basic.graph;
+
+/**
+ * @author bo.peng
+ * @create 2019-11-14 17:28
+ */
+public class GraphTest {
+}
