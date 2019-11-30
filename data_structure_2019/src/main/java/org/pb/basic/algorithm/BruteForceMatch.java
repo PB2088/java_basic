3d97547c9b4313b580170c77bb2723b71cff90c2
PB915932857 <PB915932857@_@__>
2019-11-30 16:13:03 +0800

    暴力匹配算法

---
+++ data_structure_2019/src/main/java/org/pb/basic/algorithm/BruteForceMatch.java
@@ -0,0 +1,43 @@
+package org.pb.basic.algorithm;
+
+/**
+ * 暴力匹配算法
+ * @author bo.peng
+ * @create 2019-11-30 15:55
+ */
+public class BruteForceMatch {
+
+    public static void main(String[] args) {
+        String str = "AAABBCCDDEEEEEEFFFFGGGGAAAAAAASDFSFSDFSSSS";
+        String substr = "AASDFZ";
+
+        int index = search(str, substr);
+        System.out.printf("索引位置%d", index);
+    }
+
+    public static int search(String str,String substr) {
+        char[] strCharArray = str.toCharArray();
+        char[] substrCharArray = substr.toCharArray();
+
+        /* 指向strCharArray */
+        int strIndex = 0;
+        /* 指向substrCharArray */
+        int substrIndex = 0;
+
+        while (strIndex < strCharArray.length && substrIndex < substrCharArray.length) {
+            if (strCharArray[strIndex] == substrCharArray[substrIndex]) {
+                strIndex++;
+                substrIndex++;
+            } else {
+                strIndex = strIndex - (substrIndex -1);
+                substrIndex = 0;
+            }
+        }
+
+        if (substrIndex == substrCharArray.length) {
+            return strIndex - substrIndex;
+        }
+
+        return -1;
+    }
+}
