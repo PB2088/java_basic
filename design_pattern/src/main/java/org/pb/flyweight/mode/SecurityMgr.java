5847fcb786842b171d71e2b3ae006236ba220d02
PB915932857 <PB915932857@_@__>
2020-03-13 16:20:11 +0800

    享元模式

--- design_pattern/src/main/java/org/pb/flyweight/mode/SecurityMgr.java
+++ design_pattern/src/main/java/org/pb/flyweight/mode/SecurityMgr.java
@@ -8,11 +8,13 @@ import java.util.*;
 
 /**
  * 安全管理，单例
+ *
  * @author bo.peng
  * @create 2020-03-10 15:19
  */
 public class SecurityMgr {
-    private SecurityMgr() {}
+    private SecurityMgr() {
+    }
 
     private static class SecurityMgrInstance {
         public static final SecurityMgr INSTANCE = new SecurityMgr();
@@ -22,27 +24,28 @@ public class SecurityMgr {
         return SecurityMgrInstance.INSTANCE;
     }
 
-    private Map<String, List<AuthorizationFlyweight>> map = new HashMap<>();
+    private Map<String, List<Flyweight>> map = new HashMap<>();
 
     /**
      * 模拟登陆的功能
+     *
      * @param user
      */
     public void login(String user) {
         /* 登陆时就需要把用户所拥有的权限，从数据库中取出来，存入到缓存中 */
-        List<AuthorizationFlyweight> authorizationFlyweights = selectByUser(user);
-        map.put(user,authorizationFlyweights);
+        List<Flyweight> authorizationFlyweights = selectByUser(user);
+        map.put(user, authorizationFlyweights);
     }
 
-    private List<AuthorizationFlyweight> selectByUser(String user) {
-        List<AuthorizationFlyweight> authorizationFlyweights = new ArrayList<>();
+    private List<Flyweight> selectByUser(String user) {
+        List<Flyweight> authorizationFlyweights = new ArrayList<>();
 
         TestDB.colDB.stream().forEach(s -> {
             String[] ss = s.split(",");
-            if (Objects.equals(ss[0],user)) {
-                AuthorizationFlyweight am  = new AuthorizationFlyweight(user);
+            if (Objects.equals(ss[0], user)) {
+                Flyweight flyweight = FlyweightFactory.getInstance().getFlyweight(ss[1] + "," + ss[2]);
 
-                authorizationFlyweights.add(am);
+                authorizationFlyweights.add(flyweight);
             }
         });
 
@@ -51,22 +54,23 @@ public class SecurityMgr {
 
     /**
      * 判断某个用户对某个安全实例是否拥有某种权限
-     * @param user 被检测权限的用户
+     *
+     * @param user           被检测权限的用户
      * @param securityEntity 安全实体
-     * @param permit 权限
+     * @param permit         权限
      * @return true 表示拥有相应的权限,false表示没有相应的权限
      */
-    public boolean hasPermit(String user,String securityEntity,String permit) {
-        List<AuthorizationFlyweight> authorizationFlyweights = map.get(user);
+    public boolean hasPermit(String user, String securityEntity, String permit) {
+        List<Flyweight> authorizationFlyweights = map.get(user);
 
         if (ToolsUtils.isEmpty(authorizationFlyweights)) {
-            System.out.printf("%s没有登陆或没有被分配任何权限\n",user);
+            System.out.printf("%s没有登陆或没有被分配任何权限\n", user);
             return Boolean.FALSE;
         }
 
-        for (AuthorizationFlyweight authorizationFlyweight : authorizationFlyweights) {
-            System.out.println("AuthorizationFlyweight==" + authorizationFlyweight);
-            if (authorizationFlyweight.match(securityEntity,permit)) {
+        for (Flyweight flyweight : authorizationFlyweights) {
+            System.out.println("AuthorizationFlyweight==" + flyweight);
+            if (flyweight.match(securityEntity, permit)) {
                 return Boolean.TRUE;
             }
         }
